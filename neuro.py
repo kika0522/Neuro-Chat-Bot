dbed7f9cecf997d7974c74de8ecf7ae2c77b04f3
Kika <ryanhallus@gmail.com>
2023-09-18 11:46:25 +0500

    добавил neuro.txt, в котором содержится контекст

--- neuro.py
+++ neuro.py
@@ -1,28 +1,34 @@
 import openai
 from tts import tts
 
+INPUT_PATH = 'prompt.txt'
 API_KEY = "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"
 openai.api_key = API_KEY
+
+with open('neuro.txt') as f:
+    prompt = f.readline()
+
+with open(INPUT_PATH) as f: #get last line of prompt file
+    for line in f:
+        pass
+    user_input = line
+    
 
 def neuro_request(prompt, max_tokens=256):
     response = openai.Completion.create(
         model='text-davinci-003', 
-        max_tokens=256,
-        prompt=last_line
+        max_tokens=max_tokens,
+        prompt=prompt
         )
     answer = response["choices"][0]["text"]
     return(answer)
 
-with open('prompt.txt') as f:
-    for line in f:
-        pass
-    last_line = line
 
-    with open('log.txt', "w") as log:
-
-        print(f"PROMPT: {last_line}\n\n")
-        answer = neuro_request(prompt=last_line)
-        answer = str.join(" ", answer.splitlines()) # Убираем переходы на следующую строку.
-        print((f"ANSWER: {answer}"))
-        tts(answer)
-        log.write(f'USER: {last_line}\nYOU:{answer}')
+with open('log.txt', "a") as log:
+    prompt+=user_input
+    print(f"PROMPT: {prompt}\n\n")
+    answer = neuro_request(prompt=prompt)
+    answer = str.join(" ", answer.splitlines()) # Убираем переходы на следующую строку.
+    print((f"ANSWER: {answer}"))
+    #tts(answer)
+    log.write(f'USER: {user_input}\nYOU:{answer}\n\n')
